41b980e8ee6a4bd5a427512cc7d0910a7d7af82f
bbombb <bbombb.bbombb@gmail.com>
2023-10-09 11:42:04 +0700

    add sql

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mysql = require('mysql');
+const ejs = require('ejs');
 
-app.get("/", (req, res) => {
-    res.send("Hello World! edit some data");
+// app.get("/", (req, res) => {
+//     res.send("Hello World!");
+// });
+
+const connection = mysql.createConnection({
+    host: 'srv595.hstgr.io',
+    user: 'u232952187_nodejstest',
+    password: 'VA60PT$GktGD',
+    database: 'u232952187_nodejstest'
+});
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database:', err);
+        return;
+    }
+    console.log('Connected to database');
+});
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    connection.query('SELECT * FROM items', (err, rows) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            return;
+        }
+        // console.log(rows);
+        res.render('index', {
+            data: rows
+        });
+    });
 });
 
 app.listen(port, () => {
