d183681a6c67761a0cc4ec7a1b7d2464682c7b5b
bbombb <bbombb.bbombb@gmail.com>
2023-10-09 12:37:04 +0700

    fix mysql error

--- index.js
+++ index.js
@@ -1,43 +1,37 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const mysql = require('mysql');
+// const mysql = require('mysql');
+const mysql = require('mysql2/promise'); // Import mysql2
+
 const ejs = require('ejs');
 require('dotenv').config();
 
 // app.get("/", (req, res) => {
 //     res.send("Hello World!");
 // });
-const databaseConfig = {
+const pool = mysql.createPool({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME
-};
-const connection = mysql.createConnection(databaseConfig);
-
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to database:', err);
-        return;
-    }
-    console.log('Connected to database');
+    database: process.env.DATABASE_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
-
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-    connection.query('SELECT * FROM items', (err, rows) => {
-        if (err) {
-            console.error('Error executing query:', err);
-            return;
-        }
-        // console.log(rows);
+app.get('/', async (req, res) => {
+    try {
+        const [rows, fields] = await pool.execute('SELECT * FROM items'); // Use pool.execute() for queries
+        // res.json(rows);
         res.render('index', {
             data: rows
         });
-    });
+    } catch (error) {
+        console.error('Error executing MySQL query:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
+app.set('view engine', 'ejs');
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
